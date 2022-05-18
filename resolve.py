710bd84091a39c213b9257d71281692ee9985bb0
GoochD4 <31257516+GoochD4@users.noreply.github.com>
2022-05-18 14:02:58 -0500

    Update resolve.py

--- resolve.py
+++ resolve.py
@@ -3,15 +3,20 @@ from ast import literal_eval
 from urllib import parse
 import socket
 import sys
+import os
 #Open text file with a list of urls read the lines and assign a variable
 file = open('dns.txt', 'r')
 lines = file.readlines()
 
+#delete previous version of address file.
+os.remove('address.html')
+
 #iterate through lines variable, print value to screen and write value to text file
 for index, line in enumerate(lines):
-    print(line.strip())
+    # print(line.strip())
     data = socket.gethostbyname(line.strip())
     print((data), file=open("address.html", "a"))
+    # print(data)
     file.close()
 
 file.close()
